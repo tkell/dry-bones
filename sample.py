4b181cfc58ed1cdb5d365e1dbf5b658fa42d5177
Thor Kell <thor@tide-pool.ca>
2015-07-25 12:26:04 -0400

    removing more unused code

--- sample.py
+++ sample.py
@@ -6,13 +6,10 @@
 # between Leap Motion and you, your company or other organization.             #
 ################################################################################
 
-
 import sys
 import thread
 import time
 import Leap
-from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
-
 
 class SampleListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
@@ -24,12 +21,6 @@ class SampleListener(Leap.Listener):
 
     def on_connect(self, controller):
         print "Connected"
-
-        # Enable gestures
-        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
-        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
-        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
-        controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
 
     def on_disconnect(self, controller):
         # Note: not dispatched when running in a debugger.
@@ -47,7 +38,6 @@ class SampleListener(Leap.Listener):
 
         # Get hands
         for hand in frame.hands:
-
             handType = "Left hand" if hand.is_left else "Right hand"
 
             print "  %s, id %d, position: %s" % (
@@ -63,16 +53,8 @@ class SampleListener(Leap.Listener):
                 normal.roll * Leap.RAD_TO_DEG,
                 direction.yaw * Leap.RAD_TO_DEG)
 
-            # Get arm bone
-            arm = hand.arm
-            print "  Arm direction: %s, wrist position: %s, elbow position: %s" % (
-                arm.direction,
-                arm.wrist_position,
-                arm.elbow_position)
-
             # Get fingers
             for finger in hand.fingers:
-
                 print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
                     self.finger_names[finger.type],
                     finger.id,
@@ -88,26 +70,8 @@ class SampleListener(Leap.Listener):
                         bone.next_joint,
                         bone.direction)
 
-        # Get tools
-        for tool in frame.tools:
-            print "  Tool id: %d, position: %s, direction: %s" % (
-                tool.id, tool.tip_position, tool.direction)
-
         if not (frame.hands.is_empty and frame.gestures().is_empty):
             print ""
-
-    def state_string(self, state):
-        if state == Leap.Gesture.STATE_START:
-            return "STATE_START"
-
-        if state == Leap.Gesture.STATE_UPDATE:
-            return "STATE_UPDATE"
-
-        if state == Leap.Gesture.STATE_STOP:
-            return "STATE_STOP"
-
-        if state == Leap.Gesture.STATE_INVALID:
-            return "STATE_INVALID"
 
 def main():
     # Create a sample listener and controller
